ce3a86357e25913b3b9a83481dc978a932a40864
juno <juno@krasava.fi>
2014-10-09 07:12:39 +0300

    start

---
+++ fi_FIfortune_mobi/app/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+
+var app = angular.module('fi_FIfortune_mobi', []);
+
+//var sampleApp = angular.module('GwAngularJS_desk', ['ngRoute','SeksiChattiController','SeksiChattiFilters','uuids']);
+// 
+//sampleApp.config(['$routeProvider',
+//  function($routeProvider) {
+//    $routeProvider.
+//        when('/SeksiChatti', {
+//        templateUrl: 'templates/SeksiChatti.html',
+//        controller: 'LoadCtrl'
+//      }).
+//      otherwise({
+//        redirectTo: '/SeksiChatti'
+//      });
+//}]);
